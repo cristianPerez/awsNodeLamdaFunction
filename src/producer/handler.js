750d13be62630c367a44d63172c4aa01204f4eb6
Cristian Perez <cperez354@gmail.com>
2017-12-22 00:21:05 -0500

    chore: base structure of lambda function ready.
    base structure of lambda function ready.

--- src/producer/handler.js
+++ src/producer/handler.js
@@ -5,18 +5,46 @@
 
 'use strict';
 
+/**
+ * Log for errors.
+ */
+const { error, log } = console;
+const logic = require('./logic');
 
 /**
  * Function that will be used in the serverless file and will be send as a lambda function,
- * which will use the "logic" function to create the database in dynamo.
+ * which will use the "logic" this function is using for read and proces one CSV file hosted on S3.
  * @param {parameter} event - AWS uses to pass in event data to the handler.
  * @param {parameter} context - Provide your handler the runtime information of the Lambda function that is executing.
  * @param {parameter} callback - Return information to the caller, otherwise return value is null.
  */
 
-module.exports.handler = (event, context, callback) => {
+module.exports.producer = (event, context, callback) => {
 
-    const logicComplet = logic(getSales, getOrder);
-    logicComplet(event, context, callback);
+    try {
+
+        const body = {
+            Bucket: event.bucket,
+            Key: event.key
+        };
+        logic.readData(body, function (err, data) {
+
+            if (err) {
+
+                error('There is an error reading the data: %s', err.message);
+                return callback(err, null);
+
+            }
+            log('Data saved succesfully', data);
+            return callback(null, data);
+
+        });
+
+    } catch (err) {
+
+        error('There is an error: %s', err);
+        return callback(err, null);
+
+    }
 
 };
---
+++ src/producer/logic.js
@@ -0,0 +1,18 @@
+/**
+ * Logic module.
+ * @module logic
+ */
+
+'use strict';
+
+/**
+ * Delete the database if exists and create a new one that contains the information brought from the "done" function.
+ * @param {parameter} body - Request body.
+ * @param {parameter} callback - return the process.
+ */
+
+exports.readData = function (body, callback) {
+
+    callback(null, body);
+
+};
